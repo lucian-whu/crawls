b2e1b445deeeb02acb0f19156838795b2145ca30
lucian-whu <lucian2015@qq.com>
2017-11-21 22:35:57 +0800

    add getdetailurl from wanfang

--- cnkiCrawler/getDetailUrls.py
+++ cnkiCrawler/getDetailUrls.py
@@ -12,7 +12,7 @@ browser = webdriver.Chrome(executable_path="C:\\Users\\lx201\\Desktop\\GithubPro
 
 # 将文章的URL写入文件links
 def write_urls(urls):
-    with open("links", 'a', encoding='utf-8') as f:
+    with open("detail_links", 'a', encoding='utf-8') as f:
         for url in urls:
             f.write(url + '\n')
 
@@ -21,17 +21,18 @@ start = "http://c.wanfangdata.com.cn/periodical/qbxb/2017-8.aspx"
 for i in range(0, 158):
     browser.get(start)
     t_selector = Selector(text=browser.page_source)
-    urls = t_selector.xpath('//div[@class="Content_div_detail"]/ul/li/a[1]/@href').extract()
+    urls = t_selector.xpath('//a[@class="qkcontent_name"]/@href').extract()
+
     # 找到下一页的链接
     next_url = t_selector.xpath('//div[@class="Content_div_detail"]/p/a/@href').extract()
-    print(next_url) # 下载文章的链接
     # 将链接写入文件
-    # write_urls(urls)
+    write_urls(urls)
 
     # 休息8秒钟从下一页继续爬取
     if len(next_url) is not 0:
         import time
-        time.sleep(5)
+        time.sleep(3)
         start = "http://c.wanfangdata.com.cn" + str(next_url[0])
     else:
         browser.quit()
+        print("work done!")
